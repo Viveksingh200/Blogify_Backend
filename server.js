680177db1fc25581841fe5be71d12177ec28f30b
Vivek Singh <vviveksingh8874@gmail.com>
2026-02-09 11:15:21 +0530

    cors error fixed

--- server.js
+++ server.js
@@ -10,9 +10,16 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
 // Routes
 app.use("/api/users", userRoutes);
