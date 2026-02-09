57e7dc02128ba8582e7fe2a36bb7d3c63dd4d84f
Vivek Singh <vviveksingh8874@gmail.com>
2026-02-09 14:47:33 +0530

    origin added

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["blogify-blogs.up.railway.app"],
     credentials: true,
   })
 );
